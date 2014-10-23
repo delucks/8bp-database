e22e0ab5a893f83f78d7704776a900a60ca0d735
delucks <jamie.delucks@gmail.com>
2014-10-22 22:37:48 -0400

    Initial commit, discog scraping just about down

---
+++ 8bp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+from ParserCommon import docurl
+
+def scrape_disco():
+  disco_url = "http://www.8bitpeoples.com/discography?show=all"
+  r = docurl(disco_url)
+  rootsoup = BeautifulSoup(r.text)
+  results = []
+  for index,item in enumerate(rootsoup.find_all('div','discographyBlock')):
+    if (item.find('div','albumName')) is not None:
+      disco_record = {}
+      disco_record['albumname'] = item.find('div','albumName').text
+      disco_record['artist'] = item.find('div','albumArtist').text
+      disco_record['artistlink'] = item.find('div','albumArtist').find('a').get('href')
+      disco_record['tracks'] = []
+      for trackentry in item.find_all('a','albumTrack'):
+        disco_record['tracks'].append(trackentry.text)
+      if (item.find('a','goLink') is not None):
+        disco_record['dl'] = item.find('a','goLink').get('href')
+      else:
+        disco_record['dl'] = None
+      disco_record['comment'] = item.find('div','albumComment').text
+      disco_record['release_id'] = item.find('div','albumId').text
+      results.append(disco_record)
+  return results
+
+print scrape_disco()
---
+++ README.md
@@ -0,0 +1,6 @@
+8bitpeoples data scraper and database
+=====================================
+
+This repository holds the work for the databases class project @pdasa and myself are working on.
+It involves getting various information from the 8bitpeoples record label and putting it into a relation DB, then writing an application to communicate with the DB.
+Woot.
