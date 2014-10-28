3651835c15f1f432b5e430dc6ca9b722f763a027
delucks <jamie.delucks@gmail.com>
2014-10-28 16:20:57 -0400

    Started adding in mysqldb connection

--- 8bp.py
+++ 8bp.py
@@ -2,7 +2,9 @@
 
 from bs4 import BeautifulSoup
 from ParserCommon import docurl
+import MySQLdb
 
+# returns an array of record dictionaries
 def scrape_disco():
   disco_url = "http://www.8bitpeoples.com/discography?show=all"
   r = docurl(disco_url)
@@ -33,9 +35,56 @@ def scrape_artists():
   bio = []
   nobio = []
   for item in rootsoup.find('div',id='artistsLeftColumn').find_all('a','links'):
+    results = {}
     bio.append(item.text)
   for item in rootsoup.find_all('div','noBio'):
     nobio.append(item.text)
   desc = rootsoup.find('div','texts justify').text
 
+def setup_db(cursor):
+    cursor.execute("create table if not exists Artist(
+                    artist_name		varchar(70), not null
+                    real_name		varchar(40), 
+                    location		varchar(30),
+                    website			varchar(30),
+                    bio_text		varchar(500),
+                    photo_link		varchar(30),
+                    primary key (artist_name))")
+    cursor.execute("create table if not exists Album(
+                    release_#		numeric(10,0), not null
+                    album_name 		varchar(70), not null
+                    download_link		varchar(30),
+                    #_tracks		numeric(3,0),
+                    artist_name 		varchar(70), not null
+                    description		varchar(500),
+                    art_link		varchar(30),
+                    primary key (release_#),
+                    foreign key (artist_name) references Artist
+                    on delete cascade
+                    on update cascade)")
+    cursor.execute("create table if not exists Track(
+                    album_name 		varchar(70), not null
+                    track_#			numeric(3,0),
+                    title			varchar(70),
+                    stream_link		varchar(30),
+                    primary key (album_name, track_#)
+                    foreign key (album_name) references Album
+                    on delete cascade
+                    on delete cascade)")
+
+def main():
+  db = MySQLdb.connect(host="localhost",
+                    user="xxx",
+                    passwd="xxx",
+                    db="xxx")
+  c = db.cursor()
+  for item in scrape_disco():
+    # TODO
+    c.execute("INSERT INTO Album values('"+item+"')")
+
+# for selects:
+#c.execute("SELECT * FROM BLAH")
+#for row in c.fetchall():
+#  foo
 scrape_artists()
+
