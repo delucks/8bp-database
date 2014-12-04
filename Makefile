0c3a57fe455771f4b1c5fd19656e637fe48059ce
James Luck <jamie.delucks@gmail.com>
2014-12-04 17:19:31 -0500

    Final (abandoned) version of c scraper

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 # delucks
 
 all:
-	g++ -Wall -o scrape -lcurl `xml2-config --cflags --libs` scrape.cc
-	#g++ -Wall -o scrape -lcurl scrape.cc 
+	#g++ -Wall -o scrape -I/usa/jluck/8bp-database/tidy/include/tidy -L/usa/jluck/8bp-database/tidy/lib -ltidy -Wl,-rpath,/usa/jluck/8bp-database/tidy/lib -lcurl `xml2-config --cflags --libs` scrape.cc
+	g++ -Wall -o scrape -lcurl `xml2-config --cflags --libs` scrape.cc 
 
 clean:
 	-@rm scrape
--- scrape.cc
+++ scrape.cc
@@ -1,8 +1,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+
 #include <curl/curl.h>
+
 #include <libxml2/libxml/HTMLparser.h>
+
+#include "tidy/include/tidy/tidy.h"
+#include "tidy/include/tidy/buffio.h"
 
 /*
  * file:   scrape.cc
@@ -15,7 +24,7 @@
 
 /* GLOBALS */
 const char* useragent = "CISC437 C++ databot (jluck@udel.edu)";
-const char* discog_filepath = "discog.html";
+const char* discog_filepath = "fixed.html";
 const char* artist_filepath = "artist.html";
 const char* discog_url = "http://www.8bitpeoples.com/discography?show=all";
 const char* artist_url = "http://www.8bitpeoples.com/artist/bit_shifter";
@@ -40,6 +49,44 @@ CURLcode download_page(CURL* c,const char* url,FILE* fh)
 	return res;
 }
 
+//std::string CleanHTML(const std::string &html){
+//	// Initialize a Tidy document
+//	TidyDoc tidyDoc = tidyCreate();
+//	TidyBuffer tidyOutputBuffer = {0};
+//
+//	// Configure Tidy
+//	// The flags tell Tidy to output XML and disable showing warnings
+//	bool configSuccess = tidyOptSetBool(tidyDoc, TidyXmlOut, yes)
+//		&& tidyOptSetBool(tidyDoc, TidyQuiet, yes)
+//		&& tidyOptSetBool(tidyDoc, TidyNumEntities, yes)
+//		&& tidyOptSetBool(tidyDoc, TidyShowWarnings, no);
+//
+//	int tidyResponseCode = -1;
+//
+//	// Parse input
+//	if (configSuccess)
+//		tidyResponseCode = tidyParseString(tidyDoc, html.c_str());
+//
+//	// Process HTML
+//	if (tidyResponseCode >= 0)
+//		tidyResponseCode = tidyCleanAndRepair(tidyDoc);
+//
+//	// Output the HTML to our buffer
+//	if (tidyResponseCode >= 0)
+//		tidyResponseCode = tidySaveBuffer(tidyDoc, &tidyOutputBuffer);
+//
+//	// Any errors from Tidy?
+//	if (tidyResponseCode < 0)
+//		throw ("Tidy encountered an error while parsing an HTML response. Tidy response code: " + tidyResponseCode);
+//
+//	// Grab the result from the buffer and then free Tidy's memory
+//	std::string tidyResult = (char*)tidyOutputBuffer.bp;
+//	tidyBufFree(&tidyOutputBuffer);
+//	tidyRelease(tidyDoc);
+//
+//	return tidyResult;
+//}
+
 void walkTree(xmlNode * a_node)
 {
 	xmlNode *cur_node = NULL;
@@ -59,7 +106,7 @@ void walkTree(xmlNode * a_node)
 int main()
 {
 	//CURL* c = curl_easy_init();
-	FILE* discog_fh;
+	//FILE* discog_fh;
 	//discog_fh = fopen(discog_filepath,"wb");
 	//if (!discog_fh)
 	//{
@@ -84,20 +131,31 @@ int main()
 	//	fprintf(stderr,"Could not create CURL object\n");
 	//	exit(EXIT_FAILURE);
 	//}
-	discog_fh = fopen(discog_filepath,"r");
-	if (!discog_fh)
-	{
-		fprintf(stderr,"Could not open discog filepath for reading\n");
-		exit(EXIT_FAILURE);
-	}
+
+	//discog_fh = fopen(discog_filepath,"r");
+	//if (!discog_fh)
+	//{
+	//	fprintf(stderr,"Could not open discog filepath for reading\n");
+	//	exit(EXIT_FAILURE);
+	//}
 	htmlParserCtxtPtr parser = htmlCreatePushParserCtxt(NULL,NULL,NULL,0,NULL,XML_CHAR_ENCODING_UTF8);
 	htmlCtxtUseOptions(parser, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
 	//htmlDocPtr foo = htmlCtxtReadFd(parser,fileno(discog_fh),discog_url,NULL,0);
-	htmlDocPtr foo = htmlReadFd(fileno(discog_fh),discog_url,NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+	std::ifstream t(discog_filepath);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	std::string cleaned = buffer.str();
+	const char* contents = cleaned.c_str();
+	xmlDoc* foo = htmlReadMemory(contents,sizeof(contents),NULL,NULL,HTML_PARSE_RECOVER|HTML_PARSE_NONET|HTML_PARSE_NOIMPLIED);
+	//xmlDoc* foo = htmlReadMemory(contents,sizeof(contents),NULL,NULL,HTML_PARSE_RECOVER|HTML_PARSE_NOWARNING|HTML_PARSE_NOERROR|HTML_PARSE_NONET|HTML_PARSE_NOIMPLIED); //htmlReadFd(fileno(discog_fh),NULL,NULL,0);
 	if (foo == NULL)
 	{
 		fprintf(stderr,"XML File reading failed!\n");
 	}
-	walkTree(xmlDocGetRootElement(foo));
-	fclose(discog_fh);
+	printf("foo is of size %u\n",sizeof(foo));
+	xmlNode* rootnode = xmlDocGetRootElement(foo);
+	if (rootnode == NULL)
+		fprintf(stderr,"No root node\n");
+	walkTree(rootnode);
+	//fclose(discog_fh);
 }
